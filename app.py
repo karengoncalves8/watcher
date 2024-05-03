31d34b7ad5476cc4fcc43d270681c7dcb05c10a0
karengoncalves8 <karengoncalves128@gmail.com>
2024-05-03 09:54:50 -0300

    starting bd integration

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -14,8 +22,12 @@ def movies():
 def series():
     return render_template('series.html')
 
-@app.route('/sign')
-def sign():
-    return render_template('sign.html')
+@app.route('/signIn')
+def signIn():
+    return render_template('signIn.html')
+
+@app.route('/signUp')
+def signUp():
+    return render_template('signUp.html')
 
 app.run(debug=True)
