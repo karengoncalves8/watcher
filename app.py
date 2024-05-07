a83a3d479f29cf76bde2037e15f00661c16bb762
karengoncalves8 <karengoncalves128@gmail.com>
2024-05-07 08:44:12 -0300

    login function

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_PASSWORD'] = 'fatec'
+app.config['MYSQL_DB'] = 'watcher'
  
 mysql = MySQL(app)
+
+app.secret_key = 'your secret key'
+
+    # cursor = mysql.connection.cursor()
+    # cursor.execute('select * from Users')
+    # output = cursor.fetchall()
+    # return str(output)
 
 @app.route('/')
 def index():
@@ -29,5 +36,38 @@ def signIn():
 @app.route('/signUp')
 def signUp():
     return render_template('signUp.html')
+
+@app.route('/register', methods = ['POST', 'GET'])
+def register():
+    msg = ''
+    if request.method == 'POST':
+        Name = request.form['Name']
+        Email = request.form['Email']
+        Password = request.form['Password']
+        cursor = mysql.connection.cursor()
+        cursor.execute('''INSERT INTO Users (Name, Email, Password) VALUES(%s, %s, %s)''', (Name, Email, Password))
+        mysql.connection.commit()
+        cursor.close()
+        msg = 'You have successfully registered !' 
+    return render_template('signUp.html', msg = msg)
+    
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    msg = ''
+    if request.method == 'POST':
+        Email = request.form['Email']
+        Password = request.form['Password']
+        cursor = mysql.connection.cursor()
+        cursor.execute('''SELECT * FROM Users WHERE Email = %s AND Password = %s''', (Email, Password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account[0]
+            session['username'] = account[1]
+            session['email'] = account[2]
+            msg = f'Logged in successfully! Welcome {session['username']}'
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('signIn.html', msg = msg) 
 
 app.run(debug=True)
